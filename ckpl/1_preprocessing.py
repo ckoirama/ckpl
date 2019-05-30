976afcf32c6e07a6dd3aed56a681449b07ecbc1e
Edgar Ortiz <ed.ortizm@gmail.com>
2019-05-30 13:34:33 -0400

    Saving directory list with requested parameters in memory using a pandas data frame

--- ckpl/1_preprocessing.py
+++ ckpl/1_preprocessing.py
@@ -1,5 +1,9 @@
+import os
+import pandas as pd
+import astropy.io.fits as fits
+from tkinter import Tk, filedialog
 
-def run(ctx):
+def imgs_list():
     """
     In this stage the idea is mainly identify the image: science, calibration
     (bias, flat, dark), filter (r, g, i, L), exposure time, date, etc.
@@ -10,4 +14,40 @@ def run(ctx):
         .outdata: a directory with the outcoming data
     :return:
     """
-    print("sin manos!")
+
+    # Dialog window to look for the directory path through GUI
+    root = Tk()
+    root.withdraw() #use to hide tkinter window
+
+    cwd = os.getcwd()
+    imgs_dir = filedialog.askdirectory(\
+                                       parent=root, initialdir=cwd,\
+                                       title='Please select images\' directory')
+    
+    #List for objects, to convert later to a data frame
+    
+    cols = ['File Name', 'IMAGETYP', 'FILTER', 'OBJECT', 'EXPTIME', 'DATE-OBS']
+    os.chdir(imgs_dir)
+    files_list = os.listdir()
+    objs = []
+    
+    for file in files_list:        
+        hdu = fits.open(file)
+        header = hdu[0].header
+        #parameter we want to list for our pipeline
+        obj = header['OBJECT']
+        exp_t = header['EXPTIME']
+        date = header['DATE-OBS']
+        im_type = header['IMAGETYP']
+        
+        if 'FILTER' in header:
+            filt = header['FILTER']
+        else:
+            filt = 'N.A'
+        os.chdir(cwd)
+        objs.append([file,im_type,filt,obj,str(exp_t),date])
+        hdu.close(file)
+
+    df = pd.DataFrame(objs, columns = cols)
+
+    return(df)
