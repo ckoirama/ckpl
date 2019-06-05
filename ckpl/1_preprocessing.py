50af8256ff1271a16aa1ca4f2c12e028747695d7
= <r.gnzlz.cstll@gmail.com>
2019-06-05 18:13:53 -0400

    Merge

--- ckpl/1_preprocessing.py
+++ ckpl/1_preprocessing.py
@@ -1,5 +1,9 @@
+import os
+import pandas as pd
+import astropy.io.fits as fits
+from tkinter import Tk, filedialog
 
-def run(ctx):
+def lst(im_dir):
     """
     In this stage the idea is mainly identify the image: science, calibration
     (bias, flat, dark), filter (r, g, i, L), exposure time, date, etc.
@@ -10,4 +14,43 @@ def run(ctx):
         .outdata: a directory with the outcoming data
     :return:
     """
-    print("sin manos!")
+
+    #List for objects, to convert later to a data frame
+    
+    cols = ['File Name', 'IMAGETYP', 'FILTER', 'OBJECT', 'EXPTIME', 'DATE-OBS']
+    os.chdir(im_dir)
+    files_list = os.listdir()
+    objs = []
+    
+    for file in files_list:        
+        hdu = fits.open(file)
+        header = hdu[0].header
+        #parameter we want to list for our pipeline
+        obj = header['OBJECT']
+        exp_t = header['EXPTIME']
+        date = header['DATE-OBS']
+        im_type = header['IMAGETYP']
+        
+        if 'FILTER' in header:
+            filt = header['FILTER']
+        else:
+            filt = 'N.A'
+
+        objs.append([file,im_type,filt,obj,str(exp_t),date])
+        hdu.close(file)
+
+    df = pd.DataFrame(objs, columns = cols)
+
+    return(df)
+
+def lst_gui():
+    # Dialog window to look for the directory path through GUI
+    root = Tk()
+    root.withdraw() #use to hide tkinter window
+
+    cwd = os.getcwd()
+    im_dir = filedialog.askdirectory(\
+                                       parent=root, initialdir=cwd,\
+                                       title='Please select images\' directory')
+    
+    return(lst(im_dir))
