2f7a2d91f327eb0d763b6c7d95f1988203298416
= <r.gnzlz.cstll@gmail.com>
2019-06-12 19:15:01 -0400

    Adding the reduction for the sci images using the calib data

--- ckpl/reduction.py
+++ ckpl/reduction.py
@@ -2,8 +2,10 @@ import click
 import numpy as np
 from . import preprocessing
 from astropy.io import fits
+from astropy.table import Table
 import pathlib
 from os import path
+import ntpath
 
 
 @click.command()
@@ -18,7 +20,50 @@ def cli(imdir, outdir):
     pathlib.Path(calib_path).mkdir(parents=True, exist_ok=True)
     pathlib.Path(reduced_path).mkdir(parents=True, exist_ok=True)
 
-    masterbias, masterdark, masterflats = reduct(preprocessing.ls(imdir))
+    table = preprocessing.ls(imdir)
+
+    masterbias, masterdark, masterflats, table_reduced = reduction(imdir, outdir, calib_path, reduced_path, table)
+
+    click.echo(table_reduced)
+
+    return masterbias, masterdark, masterflats, table_reduced
+
+
+def reduction(imdir, outdir, calib_path, reduced_path, table):
+    """
+
+    :param df: pandas.DataFrame
+    :return:
+    """
+
+    filenames_bias = np.array(table[table['imagetype'] == 'bias']['filename'])
+    masterbias = make_masterbias(filenames_bias) if len(filenames_bias) > 0 else None
+
+    table_darks = table[table['imagetype'] == 'dark']
+    masterdark = make_masterdark(masterbias, table_darks) if len(table_darks) > 0 else None
+
+    bands_flat = np.unique(table[table['imagetype'] == 'flat']['band'])
+
+    images_flat_by_band = {
+        band: table[(table['imagetype'] == 'flat') * (table['band'] == band)]
+        for band in bands_flat
+    }
+    #images_flat_by_band = {}
+    #for band in bands_flat:
+#        images_flat_by_band[band] = table[(table['imagetype'] == 'flat') * (table['band'] == band)]
+
+    masterflats = {
+        band: make_master_flat(images_flat, masterbias, masterdark) if masterdark is not None else None
+        for band, images_flat in images_flat_by_band.items()
+    }
+
+    bands_sci = np.unique(table[table['imagetype'] == 'light']['band'])
+    images_sci_by_band = {
+        band: table[(table['imagetype'] == 'light') * (table['band'] == band)]
+        for band in bands_sci
+    }
+
+    table_reduced = reducing_sci_by_band(table, reduced_path, masterbias, masterdark, masterflats)
 
     fits.PrimaryHDU(masterbias).writeto(path.join(calib_path, 'masterbias.fits'), overwrite=True)
     fits.PrimaryHDU(masterdark).writeto(path.join(calib_path, 'masterdark.fits'), overwrite=True)
@@ -29,42 +74,7 @@ def cli(imdir, outdir):
         in masterflats.items()
     ]
 
-
-def reduct(table):
-    """
-
-    :param df: pandas.DataFrame
-    :return:
-    """
-
-    filenames_bias = np.array(table[table['imagetype'] == 'bias']['filename'])
-    masterbias = make_masterbias(filenames_bias)
-
-    table_darks = table[table['imagetype'] == 'dark']
-    masterdark = make_masterdark(masterbias, table_darks)
-
-    bands = np.unique(table[table['imagetype'] == 'flat']['band'])
-
-    print(bands)
-
-    #images_flat_by_band = {
-    #    band: table[table['imagetype'] == 'flat' and table['band'] == band]
-    #    for band in bands
-    #}
-    images_flat_by_band = {}
-    for band in bands:
-        images_flat_by_band[band] = table[(table['imagetype'] == 'flat') * (table['band'] == band)]
-
-    print('images_flat_by_band', images_flat_by_band)
-
-    masterflats = {
-        band: make_master_flat(images_flat, masterbias, masterdark)
-        for band, images_flat in images_flat_by_band.items()
-    }
-
-    print(masterflats)
-
-    return masterbias, masterdark, masterflats
+    return masterbias, masterdark, masterflats, table_reduced
 
 
 def make_masterbias(filenames_bias):
@@ -88,7 +98,6 @@ def make_masterdark(masterbias, table_darks):
             darks = np.atleast_3d(data)
         else:
             darks = np.concatenate((darks, np.atleast_3d(data)), axis=2)
-        print('|', end='')
     return np.median(darks, axis=2)
 
 
@@ -101,9 +110,65 @@ def make_master_flat(table, master_bias, masterdark):
             flat = np.atleast_3d(data - master_bias - masterdark_factor)
         else:
             flat = np.concatenate((flat, np.atleast_3d(data - master_bias - masterdark_factor)), axis=2)
-        print('|', end='')
     masterflat = np.mean(flat, axis=2)
     median = np.median(masterflat)
     return masterflat / median
 
 
+def reducing_sci_by_band(table, outdir, masterbias, masterdark, masterflats):
+    """
+    para cada imagen de la tabla, tengo que hacer:
+    1. sci[i] -= masterbias
+    2. sci[i] -= masterdark * exptime[i]
+    3. sci[i] /= masterflat_{band}
+    :param table:
+    :return:
+    """
+
+    cols = ['filename', 'imagetype', 'band', 'object', 'exptime', 'date_obs',
+            'ra', 'dec', 'airmass', 'saturated', 'flag_bias', 'flag_dark', 'flag_flat', 'flag_astr', 'stack']
+    sci_list = []
+    for row in table[table['imagetype'] == 'light']:
+        hdu = fits.open(row['filename'])
+        data = np.float64(hdu[0].data)
+        band = row['band']
+
+        if masterbias is not None:
+            data -= masterbias
+
+        if masterdark is not None:
+            data -= masterdark * row['exptime']
+
+        if masterflats[band] is not None:
+            data /= masterflats[band]
+
+        image = fits.PrimaryHDU(data)
+        image.header = hdu[0].header
+
+        filename = ntpath.basename(row['filename'])
+        filename = path.join(outdir, 'sci_' + filename)
+        image.writeto(filename, overwrite=True)
+
+        sci_list.append((
+            filename,
+            'sci',
+            row['band'],
+            row['object'],
+            row['exptime'],
+            row['date_obs'],
+            row['ra'],
+            row['dec'],
+            row['airmass'],
+            row['saturated'],
+            masterbias is not None,
+            masterdark is not None,
+            masterflats[band] is not None,
+            None,
+            None
+        ))
+    return Table(
+        rows=sci_list,
+        names=cols,
+        # dtype=('S1', ''), TODO
+        meta={'name': 'Ckoirama Pipeline recuded data'}
+    )
