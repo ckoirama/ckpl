b89a9eed9847643849fb4819e000f564ed2c1325
= <r.gnzlz.cstll@gmail.com>
2019-06-12 19:12:59 -0400

    Skeleton for command line interface

--- ckpl/astrometry.py
+++ ckpl/astrometry.py
@@ -1,8 +1,19 @@
 import subprocess
+from . import reduction
 import click
 
+
 @click.command()
-@click.option('--blind', default=False, help='Blind Astrometry?')
+@click.option('--imdir', prompt='Directory for processed data')
+@click.option('--outdir', prompt='Directory for processed data')
+@click.option('--blind', default=False, is_flag=True, help='Blind Astrometry?')
+def cli(imdir, outdir, blind):
+    table = reduction.cli(imdir, outdir).table
+    table = table[table['imagetype' == 'light']]
+
+    print(blind)
+
+
 def astrometry(file, blind):
     """
     This function run astrometry.net software to perform blind astrometry.
@@ -14,6 +25,7 @@ def astrometry(file, blind):
     coords = ""
     scale = " --scale-units arcsecperpix --scale-low 0.46 --scale-high 0.47 "
     image = file
+
     if not blind:
         coords = "--ra 14:09:35 --dec -38:46:12 --radius 0.5"
 
